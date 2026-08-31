ea794908779d5c4b153c32ba6279bd2f73cc6f5f
jestin143 <jestinlasac@gmail.com>
2026-08-31 13:00:16 +0800

    Update orientalMindoro.js

--- src/data/orientalMindoro.js
+++ src/data/orientalMindoro.js
@@ -1,24 +1,27 @@
-// Oriental Mindoro location data ONLY - no API logic, no fake users
+// Oriental Mindoro location data
+// No API logic, no fake users.
+
+export const municipalities = [
+  { name: 'Calapan City', type: 'city' },
+  { name: 'Puerto Galera', type: 'municipality' },
+  { name: 'San Teodoro', type: 'municipality' },
+  { name: 'Baco', type: 'municipality' },
+  { name: 'Victoria', type: 'municipality' },
+  { name: 'Socorro', type: 'municipality' },
+  { name: 'Pola', type: 'municipality' },
+  { name: 'Pinamalayan', type: 'municipality' },
+  { name: 'Gloria', type: 'municipality' },
+  { name: 'Bansud', type: 'municipality' },
+  { name: 'Bongabong', type: 'municipality' },
+  { name: 'Roxas', type: 'municipality' },
+  { name: 'Mansalay', type: 'municipality' },
+  { name: 'Bulalacao', type: 'municipality' },
+  { name: 'Naujan', type: 'municipality' },
+]
 
 export const orientalMindoro = {
   province: 'Oriental Mindoro',
-  municipalities: [
-    { name: 'Calapan City', type: 'city' },
-    { name: 'Puerto Galera', type: 'municipality' },
-    { name: 'San Teodoro', type: 'municipality' },
-    { name: 'Baco', type: 'municipality' },
-    { name: 'Victoria', type: 'municipality' },
-    { name: 'Socorro', type: 'municipality' },
-    { name: 'Pola', type: 'municipality' },
-    { name: 'Pinamalayan', type: 'municipality' },
-    { name: 'Gloria', type: 'municipality' },
-    { name: 'Bansud', type: 'municipality' },
-    { name: 'Bongabong', type: 'municipality' },
-    { name: 'Roxas', type: 'municipality' },
-    { name: 'Mansalay', type: 'municipality' },
-    { name: 'Bulalacao', type: 'municipality' },
-    { name: 'Naujan', type: 'municipality' },
-  ]
+  municipalities,
 }
 
 export default orientalMindoro
